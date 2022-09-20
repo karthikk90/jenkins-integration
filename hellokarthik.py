0b9239c8db4f2cc92a7a9ffd6619547e4ecad522
karthikk90 <112143528+karthikk90@users.noreply.github.com>
2022-09-20 11:54:09 +0530

    Update hellokarthik.py
    python basic exercise copied from github

--- hellokarthik.py
+++ hellokarthik.py
@@ -2,3 +2,27 @@ print ("hello karthik this is python")
 print ("welcome to karthik learnings")
 print ("new change made")
 print ("started new print")
+# 4.1 - What is a String?
+# Solutions to review exercies
+
+
+# Exercise 1
+print('There are "double quotes" in this string.')
+
+
+# Exercise 2
+print("This string's got an apostrophe.")
+
+
+# Exercise 3
+print(
+    """This string was written on multiple lines,
+      and it displays across multiple lines"""
+)
+
+
+# Exercise 4
+print(
+    "This one-line string was written out \
+      using multiple lines"
+)
